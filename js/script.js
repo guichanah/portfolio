3656023cbd89244b81934b7d6091ca50890d1b29
guichanah <dbs0467@gmail.com>
2025-01-23 14:07:36 +0900

    0123

---
+++ js/script.js
@@ -0,0 +1,42 @@
+var menubtn = new EzenAddClass(".btn");
+
+
+var topmenu = new EzenScrollClass("header",{
+    baseline:100,
+    //markers: true
+}); 
+
+const swiper = new Swiper('#mainvisual .swiper', {
+    // Optional parameters
+    spaceBetween: 30,
+    loop: true,
+    effect: "fade",//전환효과
+    speed:800, //전환시간
+    autoplay: {//autoplay 자동전환
+    delay: 2500, //1.5초(=1500밀리초)마다 전환됨.
+    disableOnInteraction: false,},
+  
+  
+    // If we need pagination
+    pagination: {
+      el: '.page',
+      type: "fraction",
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.next',
+      prevEl: '.prev',
+    },
+});
+
+var swipers = new Swiper("#magazine .mySwiper", {
+  slidesPerView: "auto",
+  spaceBetween: 12,
+});
+
+
+
+
+
+
